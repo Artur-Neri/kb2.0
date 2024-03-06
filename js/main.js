b84b15d86b42022ed588beaeb126001d2df95e91
Artur Neri <arturnerictt@gmail.com>
2024-03-06 20:54:59 -0300

    funcionalidades do mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
-import data from './data.json' assert {type: 'json'};
+// DOM ELEMENTS
+const boardSelection = document.querySelector('#boardSelection');
+const boardSelectionOptions = document.querySelector('#boardSelectionOptions');
+const menuArrow = document.querySelector('#menuArrow');
 
-const boards = data.boards;
+// toggle
+const toggleLightDark = document.querySelector('#ldToggle');
+const toggleBall = document.querySelector('#toggleBall');
+
+boardSelection.onclick = handlesBoardSelection;
+
+toggleLightDark.onclick = handleLightDarkMode;
+
+function handlesBoardSelection() {
+    boardSelection.classList.toggle('active');
+    if (boardSelection.classList.contains('active')) {
+        openBoardOptions(); 
+    } else {
+        closeBoardOptions();
+    }
+}
+
+function openBoardOptions() {
+    boardSelectionOptions.style.display = 'inline-block';
+    menuArrow.style.transform = 'rotate(180deg)';
+}
+
+function closeBoardOptions() {
+    boardSelectionOptions.style.display = 'none';
+    menuArrow.style.transform = 'rotate(0deg)';
+}
+
+function handleLightDarkMode() {
+    toggleLightDark.classList.toggle('light');
+    if (toggleLightDark.classList.contains('light')){
+        toggleBall.style.left = '3px';
+    } else {
+        toggleBall.style.left = '22px';
+    }
+}
