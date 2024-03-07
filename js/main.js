b9ddf906eef277197633329f09bad3edd2b850d0
Artur Neri <arturnerictt@gmail.com>
2024-03-06 22:43:45 -0300

    garantindo que o mobile esteja funcionando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // DOM ELEMENTS
+const body = document.querySelector('body');
+const shadowBox = document.querySelector('#shadowBox');
 const boardSelection = document.querySelector('#boardSelection');
 const boardSelectionOptions = document.querySelector('#boardSelectionOptions');
 const menuArrow = document.querySelector('#menuArrow');
@@ -8,32 +10,32 @@ const toggleLightDark = document.querySelector('#ldToggle');
 const toggleBall = document.querySelector('#toggleBall');
 
 boardSelection.onclick = handlesBoardSelection;
+shadowBox.onclick = closeBoardOptions;
 
 toggleLightDark.onclick = handleLightDarkMode;
 
 function handlesBoardSelection() {
-    boardSelection.classList.toggle('active');
-    if (boardSelection.classList.contains('active')) {
         openBoardOptions(); 
-    } else {
-        closeBoardOptions();
-    }
 }
 
 function openBoardOptions() {
     boardSelectionOptions.style.display = 'inline-block';
     menuArrow.style.transform = 'rotate(180deg)';
+    shadowBox.style.display = 'block';
 }
 
 function closeBoardOptions() {
+    shadowBox.style.display = 'none';
     boardSelectionOptions.style.display = 'none';
     menuArrow.style.transform = 'rotate(0deg)';
 }
 
+// SWITCHES BETWEEN LIGHT AND DARK MODE
 function handleLightDarkMode() {
     toggleLightDark.classList.toggle('light');
     if (toggleLightDark.classList.contains('light')){
         toggleBall.style.left = '3px';
+
     } else {
         toggleBall.style.left = '22px';
     }
